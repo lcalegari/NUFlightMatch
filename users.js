05130542d7839f715ccb10340477d287ca4c22c0
lisa <lcalegari@users.noreply.github.com>
2022-05-28 16:42:42 -0500

    form to array works
    fixed code so submitting form enters it into user array

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 
 class User{
-    constructor(first_name, last_name,starthour, endhour, dest, phonenum, loc){
+    constructor(first_name, last_name, starthour, endhour, dest, phonenum, loc){
         this.fname = first_name;
         this.lname = last_name;
         this.starthour = starthour;
@@ -18,38 +18,24 @@ class User{
 }
 
 const Users = [];
-const Lisa = new User("Lisa", 3, 5, "Ohare", "2248770742", "North");
+const Lisa = new User("Lisa", "Calegari", 3, 5, "Ohare", "2248770742", "North");
 Users.push(Lisa);
 
 
 
 const addUserForm = document.getElementById('#info-form');
 
-//let dst = destInput;
-
-/*nameInput.addEventListener('input', updateValue(newname));
-function updateValue(e, editing) {
-    editing = e.target.value;
-}*/
-
-
-//addUserForm.addEventListener('submit', onSubmit);
-
-function updateusers(){
-    console.log(document.getElementById('#fname'));
-    var nameInput = document.getElementById('#fname').value;
-    var numberInput = document.getElementById('#fnum').value;
-    var sTimeInput = document.getElementById('#fst').value;
-    var eTimeInput = document.getElementById('#fet').value;
-    var aboutMeInput = document.getElementById('#abtme').value;
-    var destInput = document.getElementById('#dest').value;
-    var LocInput = document.getElementById('#loc').value;
-    var lastInput = document.getElementById('#lname').value;
-    let newUser = new User(nameInput, lastInput, sTimeInput, eTimeInput, destInput, numberInput, LocInput);
+function handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+    console.log({value});
+    const newUser = new User(value.firstName, value.lastname, value.startT, value.endT, value.Destination, value.number, value.PartOnCampus);
     Users.push(newUser);
     console.log(Users);
-
 }
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
 
 
 
@@ -58,3 +44,4 @@ alert('javascript working');
 
 
 //Source referenced: https://stackoverflow.com/questions/53819881/html-form-input-into-javascript-array
+//SOurce referenced: https://www.learnwithjason.dev/blog/get-form-values-as-json
